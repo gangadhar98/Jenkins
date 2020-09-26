4a91e019b684eb0e48316c09048b62bb3caa7387
msaigangadha <msaigangadha@host.docker.internal>
2020-09-26 20:34:16 +0530

    FirstCommit

---
+++ MavenJenkinsM6/src/main/java/com/dxc/jenkins/JenkinsTest.java
@@ -0,0 +1,13 @@
+package com.dxc.jenkins;
+
+public class JenkinsTest {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Guys... Welcome to DXC Technology");
+
+	}
+	
+
+}
